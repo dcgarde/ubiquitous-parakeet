1f19856e5ea79f037659890123edca74d3e73e65
dcgarde <garde.danielle@gmail.com>
2022-05-10 07:38:25 -0500

    established relationships of models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+    belongs_to :team, :user
+
+    accepts_nested_attributes_for :team
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  # has_many :projects
-  # has_many :teams
+  has_many :projects
+  has_many :teams
 
   # new function to set the password without knowing the current
   # password used in our confirmation controller.
